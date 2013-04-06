fc19a115c2b02bef82aeb5b7922d1902abeae50a
Brian Han <brianyhan@gmail.com>
2013-04-05 20:47:01 -0700

    Files sent

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abcsofmoney::Application.config.secret_token = '0da45611943b8b43e377abf55c98afb4024421b5a2ca1271f83c139477a855be310aa17271aef4e0a6135407c28bf9be8bc5bce78dc94b542bd0a237c2de79a0'
